5d2735a9fa50f8de6f10a244c09b91913806452a
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-12 13:17:18 -0800

    interface with REST Countries API, to be used in backend

---
+++ utils/countries-api.ts
@@ -0,0 +1,89 @@
+export class CountriesAPI {
+  /** 
+   * interface with the countries API, to be used in the backend
+   * doesn't take any arguments, just initialize the class and use the methods
+  */
+  baseURL: string;
+  params: any;
+
+  constructor() {
+    this.baseURL = "https://restcountries.com/v3.1/";
+    this.params = {
+      fields: [
+        'name', 'flags', 'flag',
+        'ccn3', 'cca3', 'cioc',
+        'capital', 'languages', 'latlng',
+        'region', 'subregion',
+      ].join(','),
+    };
+  }
+
+  async getCountries() {
+    const params = new URLSearchParams({
+      ...this.params 
+    });
+
+    const response = await fetch(this.baseURL + "all?" + params.toString());
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error('got unexpected status', { cause: { status: response.status, data } });
+    }
+
+    return data;
+
+  }
+
+  async getCountriesByRegion({ region } : { region: string }) {
+
+    const params = new URLSearchParams({
+      ...this.params,
+    });
+
+    if (!region) {
+      throw new Error('Need to provide provide both region and subregion to use the getCountriesByRegion method!!');
+    }
+
+    const response = await fetch(`${this.baseURL}/region/${region}?${params.toString()}`);
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error('got unexpected status', { cause: { status: response.status, data } })
+    }
+
+    return data;
+
+  }
+
+  async getCountriesBySubRegion({ subregion } : { subregion: string }) {
+    const params = new URLSearchParams({
+      ...this.params,
+    });
+
+    if (!subregion) {
+      throw new Error('Need to provide provide both region and subregion to use the getCountriesBySubRegion method!!');
+    }
+
+    const response = await fetch(this.baseURL + `subregion/${subregion}?${params.toString()}`);
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error('got unexpected status', { cause: { status: response.status, data } })
+    }
+
+    return data;
+
+  }
+
+  async getCountryById(id: string) {
+    return;
+  }
+
+  async getCountryByName(name: string) {
+    return;
+  }
+
+
+}
+
+
