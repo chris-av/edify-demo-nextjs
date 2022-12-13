0024d9528005fee06757ff45292c6675e7ed5f52
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-12 17:40:39 -0800

    implement global layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="p-4">
+      <Component {...pageProps} />
+    </div>
+  );
 }
