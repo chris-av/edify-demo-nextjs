b889c81e0d5c320f0635a355f2970ab0281e6c13
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-12 22:03:03 -0800

    static data

---
+++ data/geo-opts.ts
@@ -0,0 +1,48 @@
+export const regionOpts = [
+  "",
+  "Africa",
+  "Americas",
+  "Europe",
+  "Asia",
+  "Oceania",
+  // "Antarctic"
+];
+
+export const subregionOpts = [
+  "",
+  "Western Africa",
+  "Caribbean",
+  "South America",
+  "Northern Europe",
+  "Southern Asia",
+  "North America",
+  "Australia and New Zealand",
+  "Polynesia",
+  "Southeast Europe",
+  "Eastern Africa",
+  "South-Eastern Asia",
+  "Central Asia",
+  "Melanesia",
+  "Northern Africa",
+  "Central America",
+  "Western Asia",
+  "Eastern Asia",
+  "Southern Europe",
+  "Southern Africa",
+  "Eastern Europe",
+  "Micronesia",
+  "Western Europe",
+  "Middle Africa",
+  "Central Europe"
+];
+
+export const continentOpts = [
+  "",
+  "Africa",
+  "North America",
+  "South America",
+  "Europe",
+  "Asia",
+  "Oceania",
+  // "Antarctica"
+];
